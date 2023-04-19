dd9d210d3b51325e7400bcfeb1f1b10a0bf550ce
argaafik <jasterno.id@gmail.com>
2023-04-19 07:19:36 +0700

    first commit

---
+++ src/components/MainHero.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1 :class="$style['page-title']">To Do App Vue.js and Vite.js</h1>
+
+</template>
+
+<style module>
+.page-title {
+  font-family: 'DM Serif Display', serif;
+  font-size: 44px;
+  letter-spacing: 1.84px;
+  color: #2d2d2d;
+  margin-top: 104px;
+  margin-bottom: 20px;
+}
+
+.subtitle {
+  font-size: 1rem;
+  font-weight: bold;
+  color: #4b4949;
+  margin-top: 0;
+  margin-bottom: 30px;
+  letter-spacing: 0.67px;
+}
+</style>
